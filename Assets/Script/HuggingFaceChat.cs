a5b28e10f70870a9c9153b78fd1a05794bd5f7fe
serranitaaaaaa <valentina.huertas.serrano7@gmail.com>
2026-05-04 12:58:32 -0600

    mensaje

--- Assets/Script/HuggingFaceChat.cs
+++ Assets/Script/HuggingFaceChat.cs
@@ -1,42 +1,34 @@
-using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using TMPro;
 using System.Text;
+using TMPro;
+using UnityEngine;
 using UnityEngine.Networking;
-using Unity.Multiplayer.Center.Common;
+using UnityEngine.UI;
 
-
-public class HuggingFaceChat : MonoBehaviour
+public class HugginFaceChat : MonoBehaviour
 {
     [Header("UI")]
     public TMP_InputField inputField;
     public TMP_Text chatText;
     public Button enviarButton;
 
-    [Header("Animación")]
+    [Header("Animacion")]
     public Animator unityChanAnimator;
 
-    [Header("HuggingFaceConfig")]
+    [Header("HuggingFace Config")]
     [TextArea] public string apiKey;
 
     private const string URL = "https://router.huggingface.co/v1/chat/completions";
     private const string MODELO = "openai/gpt-oss-120b:groq";
 
-
     private const string PERSONALIDAD =
-        "Eres Unity-Chan, una asistente virtual tsundere. " +
-        "Habla con dulzura pero con orgullo, usas expresiones como hmph o baka." +
+        "Eres Unity-chan, una asistente virtual tsundere. " +
+        "Habla con dulzura pero con orgullo, usas expresiones  como hmph o baka." +
         "Tus respuestas son cortas, menos de 10 palabras" +
-        "Además de responder, analiza tu emoción y responde SOLO en ese formato JSON exacto:" +
-        "{respuesta:texto que dirás, emoción:feliz o enojada o hablar}. " +
+        "Ademas de responder, analiza tu emocion y responde SOLO en este formato JSON exacto:" +
+        "{respuesta:texto que diras,emocion:feliz o enojada o hablar}. " +
         "No agregues nada fuera del JSON";
 
-
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         enviarButton.onClick.AddListener(EnviarMensaje);
@@ -45,15 +37,13 @@ public class HuggingFaceChat : MonoBehaviour
     public void EnviarMensaje()
     {
         string mensaje = inputField.text;
-        if(string.IsNullOrWhiteSpace(mensaje))
+        if (string.IsNullOrWhiteSpace(mensaje))
         {
             return;
-
         }
         inputField.text = "";
         StartCoroutine(EnviarRequest(mensaje));
     }
-
 
     IEnumerator EnviarRequest(string mensaje)
     {
@@ -64,8 +54,8 @@ public class HuggingFaceChat : MonoBehaviour
             max_tokens = 1024,
             messages = new HFMessage[]
             {
-                new HFMessage{role="System", content = PERSONALIDAD},
-            new HFMessage{role = "user", content = mensaje}
+                new HFMessage{ role = "system", content = PERSONALIDAD},
+                new HFMessage{ role = "user", content = mensaje}
             }
         };
 
@@ -74,66 +64,96 @@ public class HuggingFaceChat : MonoBehaviour
         var request = new UnityWebRequest(URL, "POST")
         {
             uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(body)),
-            downloadHandler=new DownloadHandlerBuffer()
+            downloadHandler = new DownloadHandlerBuffer()
         };
 
         request.SetRequestHeader("Content-Type", "application/json");
-        request.SetRequestHeader("Autorization", "Bearer " + apiKey);
+        request.SetRequestHeader("Authorization", "Bearer " + apiKey);
 
         yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Error HTTP: " + request.error);
+            Debug.LogError("Body enviado " + body);
+            yield break;
+        }
+        ProcesarSolicitud(request.downloadHandler.text);
+    }
+
+    public void ProcesarSolicitud(string rawResponse)
+    {
+        try
+        {
+            var hf = JsonUtility.FromJson<HFResponse>(rawResponse);
+            string json = hf.choices[0].message.content;
+
+            var data = JsonUtility.FromJson<RespuestaAI>(json);
+
+            chatText.text = data.respuesta;
+            EjecutarAnimacion(data.emocion);
+        }
+        catch
+        {
+            chatText.text = "Unity-chan tuvo un error";
+            Debug.LogError("Error al parsear repsuesta");
+        }
+    }
+
+    public void EjecutarAnimacion(string emocion)
+    {
+        if (unityChanAnimator == null) return;
+
+        switch (emocion.ToLower())
+        {
+            case "feliz":
+                unityChanAnimator.SetTrigger("Feliz");
+                break;
+
+            case "enojada":
+                unityChanAnimator.SetTrigger("Enojada");
+                break;
+
+            default:
+                unityChanAnimator.SetTrigger("Hablar");
+                break;
+        }
     }
 
     //Request
     [System.Serializable]
-
     private class HFRequest
     {
         public string model;
         public int max_tokens;
         public HFMessage[] messages;
-
     }
 
     [System.Serializable]
-
     private class HFMessage
     {
         public string role;
         public string content;
-
     }
 
-
-    //Responsive
+    //Response
     [System.Serializable]
-
-    private class HFRsponsive
+    private class HFResponse
     {
         public Choice[] choices;
-
     }
 
-
     [System.Serializable]
-
     private class Choice
     {
-        public HFMessage[] messages;
-
+        public HFMessage message;
     }
-
     [System.Serializable]
-
     private class RespuestaAI
     {
         public string respuesta;
         public string emocion;
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
